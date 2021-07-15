f156dae939490a01bfddab95c92ae5c084be87b3
Carlos Adriano <cadriano.miranda@gmail.com>
2021-07-15 12:03:05 -0300

    refact: inicializacao do app

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 require("dotenv").config();
 
 import Koa, { Context } from "koa";
-import bodyParser from "koa-bodyparser";
 import cors from "koa2-cors";
 import logger from "koa-logger";
+import koaBody from "koa-body";
 import createRoutes from "./routes";
 import MongoDB from "./db";
 
+MongoDB.getInstance()
+  .connect()
+  .catch((e) => console.log({ error: e }));
+
 const app = new Koa();
 
-app.use(bodyParser());
+app.use(koaBody());
 app.use(
   cors({
     origin: "*",
@@ -20,9 +24,5 @@ app.use(logger());
 createRoutes(app);
 
 app.listen(5910, function () {
-  console.log("Server running on https://localhost:6040");
-
-  MongoDB.getInstance()
-    .connect()
-    .catch((e) => console.log({ error: e }));
+  console.log("Server running on https://localhost:5910");
 });
